17376c3e3fa5d0d2e89f7d777e935908b0a74c36
Shuyao zeng <zengs@oregonstate.edu>
2023-08-09 08:55:26 -0700

    Take a try...

--- hash_map_oa.py
+++ hash_map_oa.py
@@ -105,7 +105,6 @@ class HashMap:
             j_value += 1
         hash_pair = HashEntry(key, value)
         self._buckets.set_at_index(hash_key, hash_pair)
-        self._size += 1
 
     def table_load(self) -> float:
         """
@@ -138,7 +137,6 @@ class HashMap:
             hash_to_entry = old_buckets[index]
             if hash_to_entry is not None and hash_to_entry.is_tombstone is not True:
                 self.put(hash_to_entry.key, hash_to_entry.value)
-        self._size -= 1
 
     def get(self, key: str) -> object:
         """
