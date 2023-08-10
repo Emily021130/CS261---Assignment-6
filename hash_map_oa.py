66cdb79d69ba943951948dd3908e2ab5276ddf12
Shuyao zeng <zengs@oregonstate.edu>
2023-08-10 05:32:48 -0700

    Take a try...

--- hash_map_oa.py
+++ hash_map_oa.py
@@ -190,7 +190,11 @@ class HashMap:
         """
         TODO: Write this implementation
         """
-        pass
+        array_to_return = DynamicArray()
+        for index in range(self._capacity):
+            for i in self._buckets[index]:
+                array_to_return.append((i.key, i.value))
+        return array_to_return
 
     def __iter__(self):
         """
