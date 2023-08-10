1c25d2b49dcbc54c35b305fa484ca4b9c194def7
Shuyao zeng <zengs@oregonstate.edu>
2023-08-10 11:34:39 -0700

    Take a try...

--- hash_map_oa.py
+++ hash_map_oa.py
@@ -204,23 +204,21 @@ class HashMap:
         """
         TODO: Write this implementation
         """
-        """self._index = 0
-        return self"""
+        self._index = 0
+        return self
 
     def __next__(self):
         """
         TODO: Write this implementation
         """
-        """try:
+        try:
             condition = True
             while condition is True:
                 if self._buckets[self._index] is None or self._buckets[self._index].is_tombstone is True:
                     self._index += 1
-                elif self._buckets[self._index].is_tombstone is not True:
-                    condition = False
         except DynamicArrayException:
             raise StopIteration
-        return self._buckets[self._index]"""
+        return self._buckets[self._index]
 
 
 # ------------------- BASIC TESTING ---------------------------------------- #
